e81af8196b656d31962857b4685f28f502565c93
vivek-yadav <vivek.yadav@progress.com>
2023-03-27 07:02:18 +0000

    init workstation cookbook

---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: workstation
+# Recipe:: default
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
---
+++ recipes/setup.rb
@@ -0,0 +1,6 @@
+package 'tree' do
+    action :install
+end
+file '/etc/motd' do
+    content 'Property of ...'
+end
